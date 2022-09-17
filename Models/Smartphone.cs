6f45f2a28fec9f051564acd531595ee76e4aad44
Lucas Gabriel <lucasgabriel7070@outlook.com>
2022-09-16 21:44:00 -0300

    Projeto finalizado!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,35 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo;
+        protected string IMEI;
+        protected int Memoria;
 
-        public Smartphone(string numero)
+
+    // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Seu celular {Modelo} está ligando...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Seu celular {Modelo} está recebendo uma ligação...");
+        }
+
+        public void InfoCelular(){
+            Console.WriteLine("Informação sobre o celular");
+            Console.WriteLine($"Modelo {Modelo}, Número: {Numero}, IMEI: {IMEI}, Memória {Memoria}");  
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular nokia");
+Smartphone nokia = new Nokia(numero: "91919191", modelo: "Nokia", imei: "192312", memoria: 4);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+nokia.InfoCelular();
+
+Console.WriteLine($"\n");
+
+Console.WriteLine("Celular iphone");
+Smartphone iphone = new Iphone(numero: "454516", modelo: "Iphone X", imei: "151619", memoria: 8);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+iphone.InfoCelular();
